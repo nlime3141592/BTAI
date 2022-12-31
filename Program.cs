a2d512de2bdfa2a92ca33f5e84a7497a3a405eb9
nlime <nlime3141592@naver.com>
2022-12-31 17:56:50 +0900

    Fix

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace BTAI
+namespace UnchordMetroidvania
 {
+    public class DebugTable
+    {
+        public string okMessage = "OK";
+        public string noMessage = "NO";
+    }
+    public abstract class DebugNode : TaskNodeBT<DebugTable>
+    {
+        protected DebugNode(DebugTable data, int id, string name) : base(data, id, name) {}
+        protected void ShowMessage(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    public class OkDebugNode : DebugNode
+    {
+        public OkDebugNode(DebugTable data, int id, string name) : base(data, id, name) {}
+        public override InvokeResult Invoke()
+        {
+            ShowMessage(data.okMessage);
+            return InvokeResult.SUCCESS;
+        }
+    }
+    public class NoDebugNode : DebugNode
+    {
+        public NoDebugNode(DebugTable data, int id, string name) : base(data, id, name) {}
+        public override InvokeResult Invoke()
+        {
+            ShowMessage(data.noMessage);
+            return InvokeResult.SUCCESS;
+        }
+    }
     class Program
     {
         static void Main()
         {
-            Console.WriteLine("afd");
+            DebugTable data = new DebugTable();
+            OkDebugNode ok = new OkDebugNode(data, 1, "Ok");
+            NoDebugNode no = new NoDebugNode(data, 2, "No");
         }
     }
 }
